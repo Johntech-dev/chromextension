069219de1b984ea8a6bc00aa78c95f7c29a9c437
johntech-dev <johnayomide50@gmail.com>
2023-09-29 14:39:12 +0100

    5

---
+++ src/Mainflow/AddtoChromeButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Button.css'
+function AddtoChromeButton() {
+  return (
+    <div>
+      <button className='addtochrome'>Add to Chrome</button>
+    </div>
+  )
+}
+
+export default AddtoChromeButton
